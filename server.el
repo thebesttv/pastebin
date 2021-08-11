b482708d82465e9ebbfa92f5d583bc2be9a579ce
thebesttv <taojuntian@outlook.com>
2021-08-11 16:24:25 +0800

    store entries to local directory & allow different types of file

--- server.el
+++ server.el
@@ -1,43 +1,70 @@
 (eserver-register-site "/pastebin"
-                       "Paste & share.")
+  "Paste & share.")
+
+(defvar pastebin-root (expand-file-name "pastebin" (temporary-file-directory))
+  "Directory to store pastebin entries.")
+
+(defvar pastebin-entries nil
+  "An alist of astebin entries.
+Each entry is a cons struct of entry name and file name.")
+
+(mkdir pastebin-root t)
+
+(defun file-mime-type (filename)
+  (string-trim
+   (with-output-to-string
+     (with-current-buffer standard-output
+       (shell-command (combine-and-quote-strings
+                       (list "file" "-bi" filename))
+                      t nil)))))
+
+(defun pastebin-welcome ()
+  "Return a string of welcome information containing available posts."
+  (apply 'concat
+         "This is a very simple pastebin.\n"
+         "Available posts:\n"
+         (mapcar (lambda (pair)
+                   (format "  %s\n" (car pair)))
+                 pastebin-entries)))
 
 (defun httpd/pastebin (proc path query request)
-  (with-httpd-buffer proc "text/plain"
-    (if (or (string-equal path "/pastebin")
-            (string-equal path "/pastebin/"))
-        ;; list available posts
-        (progn
-          (insert "This is a very simple pastebin.\n"
-                  "Available posts:\n")
-          (mapc
-           (lambda (buffer)
-             (let ((name (buffer-name buffer)))
-               (when (and (string-prefix-p "*pastebin: " name)
-                          (string-suffix-p "*" name))
-                 (setq name (string-remove-prefix
-                             "*pastebin: "
-                             (string-remove-suffix "*" name)))
-                 (insert (concat "  " name "\n")))))
-           (buffer-list)))
-      ;; either return or store content by name
-      (let ((name (string-remove-prefix "/pastebin/" path))
-            (content (car (alist-get "Content" request
-                                     "" nil 'string-equal)))
-            buffer-name)
-        (if (string-match "[^-._/a-zA-Z0-9]+" name)
-            ;; illegal name
-            (insert "ERROR: post name invalid\n"
-                    "  Name can only ontain \"-._/\" or alphanumeric characters.\n")
-          (setq buffer-name (format "*pastebin: %s*" name))
-          (get-buffer buffer-name)
-          (if (string-empty-p content) ; content empty, then I can only return content
-              (if (get-buffer buffer-name)
-                  ;; return buffer content
-                  (insert-buffer (get-buffer buffer-name))
-                ;; no available buffer, issue warning
-                (insert "WARNING: I don't have anything to store or return.\n"))
-            ;; store content to buffer
-            (with-current-buffer (get-buffer-create buffer-name)
-              (erase-buffer)
-              (insert content))
-            (insert "content inserted to " name "\n")))))))
+  (if (or (string-equal path "/pastebin")
+          (string-equal path "/pastebin/"))
+      ;; list available posts
+      (with-httpd-buffer proc "text/plain; charset=utf-8"
+        (insert (pastebin-welcome)))
+    ;; either return or store content by name
+    (let ((entry-name (string-remove-prefix "/pastebin/" path))
+          (content (car (eserver-request-get "Content" request)))
+          file-name mime-type)
+      (if (string-match "[^-._/a-zA-Z0-9]+" entry-name)
+          ;; `entry-name' illegal
+          (with-httpd-buffer proc "text/plain; charset=utf-8"
+            (insert (format "ERROR: post name \"%s\" is invalid.\n" entry-name)
+                    "  Name can only ontain \"-._/\" or alphanumeric characters.\n"))
+        ;; `entry-name' ok
+        (if (string-empty-p content) ; content empty, then I can only return content
+            (if (null (assoc entry-name pastebin-entries))
+                ;; no such buffer, issue warning
+                (with-httpd-buffer proc "text/plain; charset=utf-8"
+                  (insert "WARNING: I don't have anything to store or return.\n"))
+              ;; buffer exists, return its content
+              (setq file-name (cdr (assoc entry-name pastebin-entries)))
+              (setq mime-type (file-mime-type file-name))
+              (with-temp-buffer
+                (set-buffer-multibyte nil)
+                (insert-file-contents-literally file-name)
+                (httpd-send-header proc mime-type 200)))
+          (let ((temporary-file-directory pastebin-root))
+            (setq file-name (make-temp-file "entry-")))
+          (with-temp-file file-name
+            ;; don't care about encoding, store as binary file
+            (set-buffer-file-coding-system 'binary)
+            (insert content))
+          (setq mime-type (file-mime-type file-name))
+          (setf (alist-get entry-name pastebin-entries
+                           nil nil 'string-equal)
+                file-name)
+          (with-httpd-buffer proc "text/plain; charset=utf-8"
+            (insert (format "Content inserted to entry: %s (%s)\n"
+                            entry-name mime-type))))))))
